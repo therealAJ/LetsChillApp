f54464ded1d897ff20c682d3664308e8d34d10a3
Alex <alexonej@gmail.com>
2016-01-30 17:12:02 -0800

    initial commit

---
+++ LetsChill/app/src/main/java/me/alexjordache/letschill/MainActivity.java
@@ -0,0 +1,13 @@
+package me.alexjordache.letschill;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
